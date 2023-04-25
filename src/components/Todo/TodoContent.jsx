70b47f39f0269f3b390f4265827592b6e3edd2f7
NANINEPRM <porramat.cha@gmail.com>
2023-04-25 15:52:37 +0700

    finish:add=todo function

--- src/components/Todo/TodoContent.jsx
+++ src/components/Todo/TodoContent.jsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import { AddTodo } from "./AddTodo";
 import { TodoHeader } from "./TodoHeader";
@@ -7,13 +8,36 @@ import mockData from "../../data/todos.json";
 export function TodoContent() {
   // # Logic
   const [todos, setTodos] = useState(mockData);
-  console.log(todos);
+  // console.log(uuidv4());
+  // console.log(uuidv4());
+  // console.log(uuidv4());
+
+  const handleAddTodo = () => {
+    // มี new todo
+    let newTodoObj = {
+      id: uuidv4(),
+      tasks: "DoHW",
+      status: false,
+      due_date: "",
+    };
+
+    // สร้าง state ใหม่
+    // update state โดย new state
+    // const newTodos = [newTodoObj, ...todos];
+    // setTodos(newTodos);
+
+    // update state โดย callback
+    setTodos((currentState) => [newTodoObj, ...currentState]);
+    // const newTodos = [newTodoObj, ...todos];
+    // setTodos(newTodos);
+  };
 
   // # UI
   return (
     <main className="content">
       <TodoHeader />
       <AddTodo />
+      <button onClick={handleAddTodo}>Test Add Todo</button>
       <TodoLists todos={todos} />
     </main>
   );
--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -50,7 +50,7 @@ export function TodoItem({ todo }) {
         <TodoForm
           submitText="Edit task"
           onSetIsShowForm={setIsEdit}
-          oldTask="old-task-name"
+          oldTask={todo.tasks}
         />
       )}
     </>
